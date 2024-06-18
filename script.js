b65d8edd830ab1a008d2d0613eaab758db5a9e24
Misba <misbahulhoque38875@gmail.com>
2024-06-18 22:19:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    });
+});
